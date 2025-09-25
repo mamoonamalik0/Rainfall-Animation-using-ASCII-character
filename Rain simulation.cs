e67ac3de20641667290acb7e8e17b14672a856c8
mamoonamalik0 <rehmankhadija144@gmail.com>
2025-09-25 12:33:05 +0500

    Add files via upload

---
+++ Rain simulation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+class RainSimulation
+{
+    static void Main()
+    {
+        string[] frames = new string[] {
+                @" .--.     .--.
+   .-(    ).-(    ).
+  (         )
+   '-(     )-'" };
+
+        Console.CursorVisible = false;
+        for (int i = 0; i < 50; i++)
+        {
+            foreach (string frame in frames)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write(frame);
+                Thread.Sleep(50);
+
+            }
+        }
+        Console.CursorVisible = true;
+    }
+}
